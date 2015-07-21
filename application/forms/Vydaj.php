9f709bd260812b02a965aa96994f783981a23789
foxyq <foxyzv@gmail.com>
2015-07-22 00:43:25 +0200

    bootstrap vydaje/add ... fixnute JS a jQuery (dufam ze na furt)

--- application/forms/Vydaj.php
+++ application/forms/Vydaj.php
@@ -23,41 +23,47 @@ class Application_Form_Vydaj extends ZendX_JQuery_Form
             ->setLabel("Dátum výdaju")
             ->addFilter('StripTags')
             ->addFilter('StringTrim')
-            ->addValidator('NotEmpty');
-
-//        $datum_vydaju = new Zend_Dojo_Form_Element_TimeTextBox('datum_vydaju_d');
-//        $datum_vydaju->setLabel('Dátum výdaju')
-//            ->setRequired(true)
-//            ->addFilter('StripTags')
-//            ->addFilter('StringTrim')
-//            ->addValidator('NotEmpty');
+            ->addValidator('NotEmpty')
+            ->setAttrib('class', 'form-control');
 
         $sklad = new Zend_Form_Element_Select('sklad_enum');
         $sklad->setMultiOptions($this->getAttrib('skladyMoznosti'));
-        $sklad->setLabel('Sklad');
+        $sklad->setLabel('Sklad')
+            ->setAttrib('class', 'form-control');
 
         $podsklad = new Zend_Form_Element_Select('podsklad_enum');
         $podsklad->setMultiOptions($this->getAttrib('podskladyMoznosti'));
-        $podsklad->setLabel('Podsklad');
+        $podsklad->setLabel('Podsklad')
+            ->setAttrib('class', 'form-control');
 
 
         $zakaznik = new Zend_Form_Element_Select('$zakaznik_enum');
         $zakaznik->setMultiOptions($this->getAttrib('zakazniciMoznosti'));
-        $zakaznik->setLabel("Zákazník");
+        $zakaznik->setLabel("Zákazník")
+            ->setAttrib('class', 'form-control');
 
 
         $prepravca = new Zend_Form_Element_Select('prepravca_enum');
         $prepravca->setMultiOptions($this->getAttrib('prepravciMoznosti'));
         $prepravca->setLabel('Prepravca');
+        $prepravca->setAttrib('class', 'form-control');
 
         $prepravca_spz = new Zend_Form_Element_Text('prepravca_spz');
         $prepravca_spz->setLabel('ŠPZ');
         $prepravca_spz->setRequired(true);
+        $prepravca_spz->setAttrib('class', 'form-control');
+
+
+
+
+
+
         //$prepravca_spz->addValidator('regex', false, array('^(B(A|B|C|J|L|N|R|S|Y)|CA|D(K|S|T)|G(A|L)|H(C|E)|IL|K(A|I|E|K|M|N|S)|L(E|C|M|V)|M(A|I|L|T|Y)|N(I|O|M|R|Z)|P(B|D|E|O|K|N|P|T|U|V)|R(A|K|S|V)|S(A|B|C|E|I|K|L|O|N|P|V)|T(A|C|N|O|R|S|T|V)|V(K|T)|Z(A|C|H|I|M|V))([ ]{0,1})([0-9]{3})([A-Z]{2})$'));
 
 
         $stroj_enum = new Zend_Dojo_Form_Element_NumberSpinner('$stroj_enum');
-        $stroj_enum->addFilter('Int');
+        $stroj_enum->addFilter('Int')
+            ->setAttrib('class', 'form-control');
 //
 //        $q_tony_merane_brutto = new Zend_Dojo_Form_Element_NumberSpinner('$q_tony_merane_brutto ');
 //        $q_tony_merane_brutto ->addFilter('Int');
@@ -81,7 +87,8 @@ class Application_Form_Vydaj extends ZendX_JQuery_Form
 //        $q_prm_vypocet->addFilter('Int');
 
         $q_tony_merane = new Zend_Form_Element_Text('q_tony_merane');
-        $q_tony_merane->setLabel('Tony merané');
+        $q_tony_merane->setLabel('Tony merané')
+            ->setAttrib('class', 'form-control');
 
 
 
@@ -90,30 +97,36 @@ class Application_Form_Vydaj extends ZendX_JQuery_Form
 
         $doklad_typ = new Zend_Form_Element_Select('doklad_typ_enum');
         $doklad_typ->setMultiOptions($this->getAttrib('dokladyTypyMoznosti'));
-        $doklad_typ->setLabel('Doklad typ');
+        $doklad_typ->setLabel('Doklad typ')
+            ->setAttrib('class', 'form-control');
 
         $material_typ = new Zend_Form_Element_Select('material_typ_enum');
         $material_typ->setMultiOptions($this->getAttrib('materialyTypyMoznosti'));
-        $material_typ->setLabel('Materiál typ');
+        $material_typ->setLabel('Materiál typ')
+            ->setAttrib('class', 'form-control');
 
 
         $material_druh = new Zend_Form_Element_Select('material_druh_enum');
         $material_druh->setMultiOptions($this->getAttrib('materialyDruhyMoznosti'));
-        $material_druh->setLabel('Materiál druh');
+        $material_druh->setLabel('Materiál druh')->setAttrib('class', 'form-control');
 
         $poznamka = new Zend_Form_Element_Text('poznamka');
-        $poznamka->setLabel('Poznámka');
+        $poznamka->setLabel('Poznámka')
+            ->setAttrib('class', 'form-control');
 
         $chyba = new Zend_Form_Element_Checkbox('chyba');
         $chyba->setLabel('Chyba');
+//            ->setAttrib('class', 'form-control');
 
         $stav_transakcie = new Zend_Form_Element_Select('stav_transakcie');
         $stav_transakcie->setMultiOptions($this->getAttrib('transakcieStavyMoznosti'));
-        $stav_transakcie->setLabel('Stav transakcie');
+        $stav_transakcie->setLabel('Stav transakcie')
+            ->setAttrib('class', 'form-control');
 
         $potvrdzujuceTlacidlo = new Zend_Form_Element_Submit('potvrdzujuceTlacidlo');
         $potvrdzujuceTlacidlo->setLabel($this->getAttrib('potvrdzujuceTlacidlo'));
-        $potvrdzujuceTlacidlo->setAttrib('id', 'submitbutton');
+        $potvrdzujuceTlacidlo->setAttrib('id', 'submitbutton')
+            ->setAttrib('class', 'form-control');
 
 
 
