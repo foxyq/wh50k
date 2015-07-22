01d2f16350e8d8c9214f9a5339f04808da7be590
foxyq <foxyzv@gmail.com>
2015-07-22 18:00:43 +0200

    fancy tables vsade, bootstrapy na formoch, ukazkovy prepocet v prijmy/add ( tony = 2*prm )

--- application/forms/Prijem.php
+++ application/forms/Prijem.php
@@ -26,37 +26,45 @@ class Application_Form_Prijem extends Zend_Form
             ->setLabel("Dátum príjmu")
             ->addFilter('StripTags')
             ->addFilter('StringTrim')
-            ->addValidator('NotEmpty');
+            ->addValidator('NotEmpty')
+            ->setAttrib('class', 'form-control');
 
 
         $sklad = new Zend_Form_Element_Select('sklad_enum');
         $sklad->setMultiOptions($this->getAttrib('skladyMoznosti'));
-        $sklad->setLabel('Sklad');
+        $sklad->setLabel('Sklad')
+            ->setAttrib('class', 'form-control');
 
         $podsklad = new Zend_Form_Element_Select('podsklad_enum');
         $podsklad->setMultiOptions($this->getAttrib('podskladyMoznosti'));
-        $podsklad->setLabel('Podsklad');
+        $podsklad->setLabel('Podsklad')
+            ->setAttrib('class', 'form-control');
 
         $dodavatel = new Zend_Form_Element_Select('dodavatel_enum');
         $dodavatel->setMultiOptions($this->getAttrib('dodavateliaMoznosti'));
-        $dodavatel->setLabel('Dodávateľ');
+        $dodavatel->setLabel('Dodávateľ')
+            ->setAttrib('class', 'form-control');
 
         $prepravca = new Zend_Form_Element_Select('prepravca_enum');
         $prepravca->setMultiOptions($this->getAttrib('prepravciMoznosti'));
-        $prepravca->setLabel('Prepravca');
+        $prepravca->setLabel('Prepravca')
+            ->setAttrib('class', 'form-control');
 
         $prepravca_spz = new Zend_Form_Element_Text('prepravca_spz');
         $prepravca_spz->setLabel('ŠPZ');
-        $prepravca_spz->setRequired(true);
+        $prepravca_spz->setRequired(true)
+            ->setAttrib('class', 'form-control');
         //$prepravca_spz->addValidator('regex', false, array('^(B(A|B|C|J|L|N|R|S|Y)|CA|D(K|S|T)|G(A|L)|H(C|E)|IL|K(A|I|E|K|M|N|S)|L(E|C|M|V)|M(A|I|L|T|Y)|N(I|O|M|R|Z)|P(B|D|E|O|K|N|P|T|U|V)|R(A|K|S|V)|S(A|B|C|E|I|K|L|O|N|P|V)|T(A|C|N|O|R|S|T|V)|V(K|T)|Z(A|C|H|I|M|V))([ ]{0,1})([0-9]{3})([A-Z]{2})$'));
 
 
 
         $q_tony_merane = new Zend_Form_Element_Text('q_tony_merane');
-        $q_tony_merane->setLabel('Tony merané');
+        $q_tony_merane->setLabel('Tony merané')
+            ->setAttrib('class', 'form-control');
 
         $q_tony_nadrozmer = new Zend_Form_Element_Text('q_tony_nadrozmer');
-        $q_tony_nadrozmer->setLabel('Tony nadrozmer');
+        $q_tony_nadrozmer->setLabel('Tony nadrozmer')
+            ->setAttrib('class', 'form-control');
 
 
         //TODO
@@ -64,30 +72,36 @@ class Application_Form_Prijem extends Zend_Form
 
         $doklad_typ = new Zend_Form_Element_Select('doklad_typ_enum');
         $doklad_typ->setMultiOptions($this->getAttrib('dokladyTypyMoznosti'));
-        $doklad_typ->setLabel('Doklad typ');
+        $doklad_typ->setLabel('Doklad typ')
+            ->setAttrib('class', 'form-control');
 
         $material_typ = new Zend_Form_Element_Select('material_typ_enum');
         $material_typ->setMultiOptions($this->getAttrib('materialyTypyMoznosti'));
-        $material_typ->setLabel('Materiál typ');
+        $material_typ->setLabel('Materiál typ')
+            ->setAttrib('class', 'form-control');
 
 
         $material_druh = new Zend_Form_Element_Select('material_druh_enum');
         $material_druh->setMultiOptions($this->getAttrib('materialyDruhyMoznosti'));
-        $material_druh->setLabel('Materiál druh');
+        $material_druh->setLabel('Materiál druh')
+            ->setAttrib('class', 'form-control');
 
         $poznamka = new Zend_Form_Element_Text('poznamka');
-        $poznamka->setLabel('Poznámka');
+        $poznamka->setLabel('Poznámka')
+            ->setAttrib('class', 'form-control');
 
         $chyba = new Zend_Form_Element_Checkbox('chyba');
         $chyba->setLabel('Chyba');
 
         $stav_transakcie = new Zend_Form_Element_Select('stav_transakcie');
         $stav_transakcie->setMultiOptions($this->getAttrib('transakcieStavyMoznosti'));
-        $stav_transakcie->setLabel('Stav transakcie');
+        $stav_transakcie->setLabel('Stav transakcie')
+            ->setAttrib('class', 'form-control');
 
         $potvrdzujuceTlacidlo = new Zend_Form_Element_Submit('potvrdzujuceTlacidlo');
         $potvrdzujuceTlacidlo->setLabel($this->getAttrib('potvrdzujuceTlacidlo'));
-        $potvrdzujuceTlacidlo->setAttrib('id', 'submitbutton');
+        $potvrdzujuceTlacidlo->setAttrib('id', 'submitbutton')
+            ->setAttrib('class', 'form-control');
 
 
         $this->addElements(array(
