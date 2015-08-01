d35750ca7b8d7eed9b73739df6781dbbf8b2ddcf
polakto <polakto@gmail.com>
2015-08-01 20:09:12 +0200

    2015_08_01_v2 - CRUD ciselnikov, merneJednotky model repoaired

---
+++ application/models/DbTable/MerneJednotky.php
@@ -0,0 +1,58 @@
+<?php
+
+class Application_Model_DbTable_MerneJednotky extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'merne_jednotky';
+
+    //na zaklade id vracia skratku mernej jednotky t, PRM, m3 ...
+    public function getSkratka($id){
+
+        $id = (int)$id;
+    	$nazov = $this->fetchRow('merne_jednotky_id = '.$id)->skratka;
+    	return $nazov;
+    }
+
+    //vracia v poli moznosti pre vypis
+    public function getMoznosti(){
+        $pole = $this->fetchAll()->toArray();
+        $moznosti = array();
+
+        foreach ($pole as $hodnota){
+            $moznosti[$hodnota['merne_jednotky_id']] = $hodnota['skratka'];
+        }
+
+        return $moznosti;
+    }
+
+    //vracia pole s mnozstvom vo vsetkych dostupnych jednotkach prepocitanych podla konverzii definovanych druhom dreva
+    //vracia pole s nasledovnymi klucmy $prepoctyArray['t'], $prepoctyArray['prm'], $prepoctyArray['m3']
+    public function getPrepoctyArray($mnozstvo, $mernaJednotkaId, $materialDruhId){
+        $materialDruhy = new Application_Model_DbTable_MaterialyDruhy();
+        $konverzie = $materialDruhy->getKonverzie($materialDruhId);
+        $prepoctyArray = array();
+
+        switch ($mernaJednotkaId){
+            case 1:
+                $prepoctyArray['t'] = $mnozstvo;
+                $prepoctyArray['prm'] = $mnozstvo * $konverzie['ton_prm'];
+                $prepoctyArray['m3'] = $mnozstvo * $konverzie['ton_m3'];
+                break;
+            case 2:
+                $prepoctyArray['prm'] = $mnozstvo;
+                $prepoctyArray['t'] = $mnozstvo * $konverzie['prm_ton'];
+                $prepoctyArray['m3'] = $mnozstvo * $konverzie['prm_m3'];
+                break;
+            case 3:
+                $prepoctyArray['m3'] = $mnozstvo;
+                $prepoctyArray['t'] = $mnozstvo * $konverzie['m3_ton'];
+                $prepoctyArray['prm'] = $mnozstvo * $konverzie['m3_prm'];
+                break;
+        }
+
+        return $prepoctyArray;
+    }
+
+
+}
+
