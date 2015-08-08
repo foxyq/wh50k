2437853c35e1750be8550af08ef2cf0caf59314a
foxyq <foxyzv@gmail.com>
2015-08-08 19:52:35 +0200

    sklady grafy funkcne + formatting

--- public/js/morris-data.js
+++ public/js/morris-data.js
@@ -1,93 +1,9 @@
 $(function() {
 
+
     // Area Chart
     Morris.Area({
         element: 'morris-area-chart-1',
-        data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
-        }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
-        }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
-        }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
-        }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
-        }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
-        }],
-        xkey: 'period',
-        ykeys: ['iphone'],
-        labels: ['iPhone'],
-        pointSize: 2,
-        hideHover: 'auto',
-        resize: true
-    });
-
-    // Area Chart
-    Morris.Area({
-        element: 'morris-area-chart-2',
-        data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
-        }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
-        }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
-        }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
-        }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
-        }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
-        }],
-        xkey: 'period',
-        ykeys: ['iphone'],
-        labels: ['iPhone'],
-        pointSize: 2,
-        hideHover: 'auto',
-        resize: true
-    });
-
-
-    // Area Chart
-    Morris.Area({
-        element: 'morris-area-chart-3',
         data: [{
             period: '2010 Q1',
             iphone: 2666,
